f879659590c9c51bbb83b69fec5b25b23b28838e
Kal EL Pratama <152152274+kalpratama@users.noreply.github.com>
2025-08-26 17:54:52 +0700

    >> PRODUCTION URLS <<
    Adjusted app URLs for production version

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-    // 'url' => env('APP_URL', 'https://repository.mncu.ac.id'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://repository.mncu.ac.id'),
 
     /*
     |--------------------------------------------------------------------------
